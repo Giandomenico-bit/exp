30bbf3c357ca13f40601115d1c1c06b2886502dd
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-22 23:17:24 +0100

    Update

--- nepero.c
+++ nepero.c
@@ -1,6 +1,6 @@
 /* Nepero
 Si scriva un programma C che approssimi il valore
-della costante e (numero di Nepero) mediante la formula:
+della costante e (numeroero di Nepero) mediante la formula:
 
 e = 1 + 1/1! + 1/2! + 1/3! + ...
 Si effettui due volte il calcolo. Nel primo caso
@@ -16,36 +16,37 @@ Quanti termini sono necessari?
 
 int main( void ){
 
-  int i;
-  float e, fact = 1;
-  float num;
-  int digit;
-  int count = 0;
+  int termine, fact, count, cifra;
+  float e, numero;
 
-  for( i = 1; i <= 10; i++ ){
-    fact = fact * i;
-    e = e + 1.0 / ( float )fact;
-  }
-
-  printf( "e =  %f\n", e );
-
-  e = 0;
   fact = 1;
+  e = 0.0;
 
-  for( i = 1; ; i++ ){
-    fact = fact * i;
-    e = e + 1.0 / ( float )fact;
-
-    num = 1.0 / ( float )fact;
-    digit = num * 1000;
-    digit = digit % 10;
-
-    if( i > 2 && digit == 0 ) break;
-    count = count + 1;
-
+  for( termine = 1; termine <= 10; termine++ ) {
+       fact = fact * termine;
+       e = e + 1.0 / ( float )fact;
   }
 
-  printf( "Sono necessari %d\n", count + 1 );
+  printf( "e = %f\n", e );
+
+  e = 0.0;
+  fact = 1;
+  count = 0;
+
+  for( termine = 1; termine < 10; termine++ ) {
+       fact = fact * termine;
+       e = e + 1.0 / ( float )fact; // calcolo la costante e
+
+       numero = 1.0 / ( float )fact; // termine aggiunto
+       cifra = numero * 1000; // mi sposto di 3 posizioni
+       cifra = cifra % 10; // calcola la terza cifra decimale
+
+       if( termine > 2 && cifra == 0 )
+            break;
+       count = count + 1;
+  }
+
+  printf( "Sono necessari %d termini\n", count + 1 );
 
   return 0;
 }
